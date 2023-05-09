26eb94898bd66aae0f07b845660c370ffbf957ec
miquelmarco <120406764+miquelmarco@users.noreply.github.com>
2023-05-09 19:38:49 -0400

    categorías dinámicas

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,4 @@
+
 let divGeneral = document.getElementById('cardContainer')
 
 function plantillaCard(obj){
@@ -21,3 +22,38 @@ function printCard(list, lugarImpresion){
 }
 
 printCard(data.events, divGeneral)
+
+// task 3
+
+let checkboxContainer = document.querySelector(`#checkContainer`)
+let datos = data.events
+
+function plantillaCheckbox(obj){
+    return `<div class="d-flex justify-content-center align-items-center">
+    <input class="ms-1" type="checkbox" id=${obj.category} value="museum">
+    <label for="${obj.category}">${obj.category}</label>
+</div>`
+}
+
+function printCheckbox(lista, checkboxContainer){
+    let template = ``
+    for (let check of lista) {
+        template += plantillaCheckbox(check)
+    }
+    checkboxContainer.innerHTML = template
+}
+
+printCheckbox(data.events, checkboxContainer)
+
+// let arrayCompleto = datos[0].category
+
+// function filtrar (array){
+// for(i = 0; i < array.length; i++){
+//     let datosFiltrados = []
+//     if(i != array[i]){
+//         datosFiltrados.push(i)
+//         console.log(datosFiltrados)
+//     }
+// }
+// }
+// filtrar(arrayCompleto)
