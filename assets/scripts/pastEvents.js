3f6c057b0cc1154f7aec4ba985b6583c84429082
miquelmarco <120406764+miquelmarco@users.noreply.github.com>
2023-05-15 13:59:40 -0400

    fetch main pages

--- assets/scripts/pastEvents.js
+++ assets/scripts/pastEvents.js
@@ -1,17 +1,64 @@
-let divGeneralPs = document.getElementById('cardContainerPe')
-let datos = data.events
 
-function plantillaCard(obj){
-    return `<div class="card mt-3 mb-3" style="width: 18rem;">
-            <img src=${obj.image} class="card-img-top p-4" alt="food fair">
-            <div class="card-body">
-            <h5 class="card-title">${obj.name}</h5>
-            <p class="cardp card-text">${obj.description}</p>
-            <h6>Price: ${obj.price}<</h6>
-            <a href="../pages/details.html?_id=${obj._id}" class="btn btn-primary btn2">See More...</a>
-            </div>
-            </div> `
-}
+// // variables
+
+let divGeneralPs = document.getElementById('cardContainerPe')
+let checkboxContainerPs = document.querySelector(`#checkboxContainerPs`)
+let inputBusquedaPs = document.querySelector(`#inputBusquedaPs`)
+let datosDeAPI
+
+// // fetch Api: https://mindhub-xj03.onrender.com/api/amazing
+
+fetch(`https://mindhub-xj03.onrender.com/api/amazing`)
+    .then(response => response.json())
+    .then(data => {
+        datosDeAPI = data
+
+        function filterDataUpcoming(evento){
+            return evento.date < data.currentDate
+        }
+        let upcomingEvents = datosDeAPI.events.filter(filterDataUpcoming)
+        printCard(upcomingEvents, divGeneralPs)
+
+        let arrayFiltrado = datosDeAPI.events.map((item) => item.category)
+        let newArrayFiltrado = [...new Set(arrayFiltrado)]
+        printCheckbox(newArrayFiltrado, checkboxContainerPs)
+    })
+    .catch(error => console.log(error))
+
+    
+    // // eventos de filtrado
+    
+    inputBusquedaPs.addEventListener('input', () => {
+        filtroDoble()
+    })
+    
+    checkboxContainerPs.addEventListener('change', () => {
+        
+        filtroDoble()
+    })
+    
+    // // templates de impresión
+    
+    function plantillaCard(obj){
+        return `<div class="card mt-3 mb-3" style="width: 18rem;">
+                <img src=${obj.image} class="card-img-top p-4" alt="food fair">
+                <div class="card-body">
+                <h5 class="card-title">${obj.name}</h5>
+                <p class="cardp card-text">${obj.description}</p>
+                <h6>Price: ${obj.price}<</h6>
+                <a href="../pages/details.html?_id=${obj._id}" class="btn btn-primary btn2">See More...</a>
+                </div>
+                </div> `
+    }
+    
+    function plantillaCheckbox(check){
+        return `<div class="d-flex justify-content-center align-items-center">
+                <input class="ms-1" type="checkbox" id='${check}' value='${check}'>
+                <label for='${check}'>${check}</label>
+                </div>`
+    }
+
+// // funciones de impresión
 
 function printCard(list, lugarImpresion){
     let template = ''
@@ -24,26 +71,6 @@ function printCard(list, lugarImpresion){
     lugarImpresion.innerHTML = template
 }
 
-function filterDataPast(evento){
-    return evento.date < data.currentDate
-}
-
-let pastEvents = data.events.filter((filterDataPast));
-
-printCard(pastEvents, divGeneralPs)
-
-// // checkbox dinámico + impresión
-
-let checkboxContainerPs = document.querySelector(`#checkboxContainerPs`)
-let inputBusquedaPs = document.querySelector(`#inputBusquedaPs`)
-
-function plantillaCheckbox(check){
-    return `<div class="d-flex justify-content-center align-items-center">
-            <input class="ms-1" type="checkbox" id='${check}' value='${check}'>
-            <label for='${check}'>${check}</label>
-            </div>`
-}
-
 function printCheckbox(lista, checkboxContainerPs){
     let template = ``
     for (let check of lista) {
@@ -52,27 +79,7 @@ function printCheckbox(lista, checkboxContainerPs){
     checkboxContainerPs.innerHTML = template
 }
 
-let arrayFiltradoPs = datos.map((item) => item.category)
-let newArrayFiltradoPs = [...new Set(arrayFiltradoPs)]
-
-printCheckbox(newArrayFiltradoPs, checkboxContainerPs)
-
-// // hacer filtros para checkbox e imput search
-
-// let checkboxContainer = document.querySelector(`#checkboxContainerPs`)
-
-// // eventos de filtrado
-
-inputBusquedaPs.addEventListener('input', () => {
-    filtroDoble()
-})
-
-checkboxContainerPs.addEventListener('change', () => {
-    
-    filtroDoble()
-})
-
-// // funciones de filtrado
+// // funciones de filtrado cards y check
 
 function filtrarSearch(array, input) {
     let filtroSearchPs = array.filter(item => item.name.toLowerCase().includes(input.toLowerCase()))
@@ -83,7 +90,6 @@ function filtrarInput (eventos, category){
     if(category.length == 0){
         return eventos
     }
-    
     return eventos.filter(evento => category.includes(evento.category))
 }
 
